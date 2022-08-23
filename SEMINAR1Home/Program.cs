3d5b628df774620e3c3aae03a1e5f7ab71952b7c
Stanyslav <stanyslavsky33@mail.ru>
2022-08-23 10:20:57 +0700

    seminar1home

---
+++ SEMINAR1Home/Program.cs
@@ -0,0 +1,107 @@
+﻿/*//Программа, на вход принимающая два числа,
+ //а на выходе показывает какое число большее, а какое меньшее.
+
+int num;
+int num2;                                                                                                                                                        //Ввод данных
+
+ // Ввод данных
+  Console.Write("Введите число первое: ");
+  num = Convert.ToInt32(Console.ReadLine());
+
+  Console.Write("Введите число второе: ");
+  num2= Convert.ToInt32(Console.ReadLine());
+
+
+ // Вычисление большего числа из двух
+
+if(num > num2 )
+    {
+    Console.WriteLine("Число первое больше чем второе = " + num );
+}
+else if(num2 > num){
+    Console.WriteLine("Число первое меньше чем второе = " + num2);
+}
+
+
+*/
+
+
+/*//Программа на вход принимает три числа 
+//и выдает максимальное из этих чисел
+
+int num1;
+int num2;
+int num3;
+
+// Ввод данных
+Console.Write("a= ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("b= ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("c= ");
+num3 = Convert.ToInt32(Console.ReadLine());
+
+// Вычисление максчисла и вывод данных
+
+if(num1 > num2 ){
+    if(num1 > num3){
+    Console.WriteLine("max = " + num1 );
+}
+else{Console.WriteLine("max = " + num3 );}
+}
+
+else if(num2 > num3){
+    Console.WriteLine("max = " + num2 );
+}
+else{
+   Console.WriteLine("max = " + num3 ); 
+}
+
+
+*/
+
+
+/*
+//программа, которая на вход принимает число и выдаёт,
+ //является ли число чётным (делится ли оно на два без остатка)
+       
+       
+       //Ввод данных, вычисление
+        {
+                Console.WriteLine("Введите число : ");
+                int a = Convert.ToInt32(Console.Read());
+                
+       //Вывод на экран         
+                if (a % 2 == 0) Console.WriteLine("ДА, четное");
+                else Console.WriteLine("НЕТ,нечетное");
+                Console.ReadLine();
+        }
+*/
+
+
+/*
+//Программа, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+int num;
+
+Console.Write("Введите число:");
+num = Convert.ToInt32(Console.ReadLine());
+
+int current = 1 ;
+
+while (current <= num){
+
+    if (current %2 == 0)
+    Console.Write(current + " ");
+current++;
+}
+
+*/
+
+
+
+
+
